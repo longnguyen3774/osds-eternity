8313e5ad0a7da466ba4e8e8ff24f9e4632b71d94
nami1713 <phuongnam2k4@gmail.com>
2024-10-29 13:28:24 +0700

    Added 2 more queries and fixed code in query.py

--- code/query.py
+++ code/query.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 from pymongo import MongoClient
-from etherscan_scraper import collect_data_page
-from etherscan_scraper import collect_data_block
+from bs4_etherscan_scraper import collect_data_page
+from bs4_etherscan_scraper import collect_data_block
 
 # Kết nối đến MongoDB
 client = MongoClient('mongodb://localhost:27017/')
@@ -45,7 +45,7 @@ get_block_with_max_eth = transactions_collection.aggregate([
     {'$group': {
             '_id': '$block',
             'totalETH': { '$sum': '$amount' }}},
-    { '$sort': { 'totalETH': -1 } },
+    { '$sort': { 'totalETH': -1 }},
     { '$limit': 1 }])
 
 for block in get_block_with_max_eth:
@@ -72,7 +72,7 @@ get_month_with_max_transaction = transactions_collection.aggregate([
                 'year': { '$year': '$age' },
                 'month': { '$month': '$age' }},
             'count': { '$sum': 1 }}},
-    { '$sort': { 'count': -1 } },
+    { '$sort': { 'count': -1 }},
     { '$limit': 1 }])
 
 for max_transaction in get_month_with_max_transaction:
@@ -94,22 +94,19 @@ count_to = transactions_collection.aggregate([
     { '$sort': { 'receive_count': -1 }},
     { '$limit': 5 }]) #giới hạn chỉ lấy 5 giao dịch
 
-for count in count_to:
-    print(count)
+for count_t in count_to:
+    print(count_t)
 
-# # 8 Tìm ngày có số lượng giao dịch cao nhất
-# print("\nNgày có số lượng giao dịch cao nhất và tổng lượng ETH giao dịch trong ngày đó:")
-# day_result = transactions_collection.aggregate([
-#     {
-#         '$group': {
-#             '_id': { '$dateToString': { 'format': '%Y-%m-%d', 'date': '$age' } },
-#             'transaction_count': { '$sum': 1 },
-#             'total_eth': { '$sum': '$amount' }}},
-#     { '$sort': { 'transaction_count': -1 } },
-#     { '$limit': 1 }])
-#
-# for day in day_result:
-#     print(f"Ngày: {r['_id']}, Số giao dịch: {r['transaction_count']}, Tổng lượng ETH: {r['total_eth']}")
+# 8 Địa chỉ gửi (from) phổ biến
+# Đếm số lần mỗi địa chỉ gửi được giao dịch
+print("\nĐịa chỉ gửi phổ biến nhất:")
+count_from = transactions_collection.aggregate([
+    { '$group': { '_id': '$from', 'receive_count': { '$sum': 1 }}},
+    { '$sort': { 'receive_count': -1 }},
+    { '$limit': 5 }]) #giới hạn chỉ lấy 5 giao dịch
+
+for count_f in count_from:
+    print(count_f)
 
 # 9 Đếm số lượng giao dịch có giá trị ETH lớn hơn trung bình
     # Tính giá trị trung bình của các giao dịch
@@ -157,12 +154,9 @@ for new in new_transaction:
 count_transaction_lower_than = transactions_collection.count_documents({ 'txn_fee': { '$lt': 0.001 }})
 print(f"\nSố lượng giao dịch có phí thấp hơn 0.001 ETH: {count_transaction_lower_than}")
 
-# # 15 Tìm tất cả giao dịch có địa chỉ nhận (to) là trống
-# print("\nGiao dịch không có địa chỉ nhận (to):")
-# null_transaction = transactions_collection.find({ 'to': { '$in': [None, '']}})
-#
-# for null_t in null_transaction:
-#     print(null_t)
+# 15 Đếm tất cả giao dịch có địa chỉ nhận (to) là trống
+null_transaction = transactions_collection.count_documents({ 'to': { '$in': [None, '']}})
+print(f"\nSố lượng giao dịch không có địa chỉ nhận (to): {null_transaction}")
 
 # 16 Lấy danh sách tất cả các địa chỉ gửi (from) không trùng lặp
 print("\nDanh sách các địa chỉ gửi không trùng lặp:")
@@ -171,17 +165,24 @@ from_address = transactions_collection.distinct('from')
 for address in from_address:
     print(address)
 
-# # 17 Tìm các giao dịch có địa chỉ nhận (to) và gửi (from) giống nhau
-# print("\nGiao dịch mà địa chỉ gửi và nhận giống nhau:")
-# get_transaction_with_same_address = transactions_collection.find({ '$expr': { '$eq': ['$from', '$to'] } })
-#
-# for same_address in get_transaction_with_same_address:
-#     print(same_address)
+# 17 Tìm các giao dịch có địa chỉ nhận (to) và gửi (from) giống nhau
+print("\nGiao dịch mà địa chỉ gửi và nhận giống nhau:")
+get_transaction_with_same_address = transactions_collection.find({ '$expr': { '$eq': ['$from', '$to'] }})
+
+for same_address in get_transaction_with_same_address:
+    print(same_address)
 
 # 18 Đếm số lượng giao dịch có giá trị ETH bằng 0
 count_transaction_equal_0 = transactions_collection.count_documents({ 'amount': 0 })
 print(f"\nSố lượng giao dịch có giá trị ETH bằng 0: {count_transaction_equal_0}")
 
+# 19 Đếm số lượng địa chỉ nhận (to) duy nhất trong toàn bộ dữ liệu
+count_to_address = len(transactions_collection.distinct('to'))
+print(f"\nSố lượng địa chỉ nhận duy nhất trong dữ liệu: {count_to_address}")
+
+# 20 Tính tổng số lượng giao dịch có giá trị ETH bằng hoặc lớn hơn 1
+transactions_equal_to_or_greater_than_1 = transactions_collection.count_documents({ 'amount': { '$gte': 1 }})
+print(f"\nSố lượng giao dịch có giá trị từ 1 ETH trở lên: {transactions_equal_to_or_greater_than_1}")
 
 # Đóng kết nối MongoDB khi hoàn thành
 client.close()
--- code/store_data.py
+++ code/store_data.py
@@ -1,3 +1,23 @@
+# from pymongo import MongoClient
+# from bs4_etherscan_scraper import collect_data_block
+#
+# client = MongoClient('mongodb://localhost:27017')
+# client.drop_database('ether_db')
+# db = client['ether_db']
+#
+# transactions_collection = db['transactions']
+#
+# last_block = 0
+# i = last_block+1
+# while True:
+#     data = collect_data_block(i)
+#     if i > 1000000:
+#         if data:
+#             transactions_collection.insert_many(data)
+#             i += 1
+#         else:
+#             break
+#     transactions_collection.insert_many(data)
 
 from pymongo import MongoClient
 from bs4_etherscan_scraper import collect_data_block
@@ -8,14 +28,8 @@ db = client['ether_db']
 
 transactions_collection = db['transactions']
 
-last_block = 0
-i = last_block+1
-while True:
-    data = collect_data_block(i)
-    if i > 1000000:
-        if data:
-            transactions_collection.insert_many(data)
-            i += 1
-        else:
-            break
-    transactions_collection.insert_many(data)
+for i in range(18908895, 18909001):
+    transactions = collect_data_block(i)
+    if not transactions:
+        continue
+    transactions_collection.insert_many(transactions)
