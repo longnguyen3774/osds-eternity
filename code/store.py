5701c96b3e26e5f0637e5a8b8bf7c1adf41fe089
Trung <trungchu0504@gmail.com>
2024-10-30 20:53:07 +0700

    cap nhat file store.py

--- code/store.py
+++ code/store.py
@@ -1,15 +1,25 @@
-from collect_data import transactions_collection
-from update import find_new_data
-from etherscan_scraper import collect_data
+from pymongo import MongoClient
+from datetime import datetime
+from bs4_etherscan_scraper import collect_data_block
 
-new_data = collect_data()
-mongo = 'mongodb://localhost:27017/'
-db = 'ether_db'
-collection = 'transactions'
-new_transactions = find_new_data(mongo, db, collection, new_data)
-if new_transactions:
-    print(f"Tìm thấy {len(new_transactions)} giao dịch mới")
-    transactions_collection.insert_many(new_transactions)
-    print("Đã lưu các giao dịch mới vào mongodb")
-else:
-    print("Không có giao dịch mới")
+def store_data(block_number):
+    data = collect_data_block(block_number)
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['ether_db']
+    transactions_collection = db['transactions']
+    if not data:
+        print(f"No data collected for block {block_number}.")
+        return 0
+    try:
+        result = transactions_collection.insert_many(data)
+        print(f"Insert {len(result.inserted_ids)} documents into {transactions_collection}")
+        return len(result.inserted_ids)
+    except Exception as e:
+        print(f"Errol inserting data into MongoDB: {e}")
+        return 0
+
+
+if __name__ == "__main__":
+    block_number = 2100720
+    insert =  store_data(block_number)
+    print(f"Total documents inserted: {insert}")
